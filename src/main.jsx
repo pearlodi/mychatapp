d56e8e7b12ba5077d4457c62ccbcb7ff7d14007d
pearlodi7@gmail.com <you@example.com>
2024-03-18 20:50:58 +0100

    create the router for the webpages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Index from './pages/Index';
+import Signup from './pages/Signup';
 import './css/main.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+{
+  path: '/',
+  element: <Index />,
+  errorElement: <div>404 not found</div>
+},
+{
+  path: '/signup',
+  element: <Signup />
+}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
