c480c668ff0b06ee8b813e357d5244d0aa93b213
pearlodi7@gmail.com <you@example.com>
2024-03-20 04:36:21 +0100

    Display list of data from the database

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,33 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 const Index = () => {
-  return (
-    <div>Index</div>
-  )
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    function getUsers() {
+        axios.get('http://localhost:80/api/user/').then(function(response) {
+            setUsers(response.data);
+        });
+    }
+
+    return (
+        <div>
+            <h1>
+            list of names in the database
+            </h1>
+                    {users.map((user, key) =>
+                        <div key={key}>
+                            <ul>
+                            <li>{user.first_name}</li>
+                            </ul>
+                        </div>
+                    )}
+        </div>
+    )
 }
 
 export default Index
